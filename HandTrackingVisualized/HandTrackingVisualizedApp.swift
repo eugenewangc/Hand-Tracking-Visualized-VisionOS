4f722b98858005a0e1e2cfe7f492659bbb68aedf
Eugene Wang <wangc@wangc.com>
2024-07-15 13:43:33 -0500

    Initial Commit

---
+++ HandTrackingVisualized/HandTrackingVisualizedApp.swift
@@ -0,0 +1,33 @@
+//
+//  HandTrackingVisualizedApp.swift
+//  HandTrackingVisualized
+//
+//  Created by Eugene Wang on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HandTrackingVisualizedApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
---
+++ HandTrackingVisualizedTests/HandTrackingVisualizedTests.swift
@@ -0,0 +1,17 @@
+//
+//  HandTrackingVisualizedTests.swift
+//  HandTrackingVisualizedTests
+//
+//  Created by Eugene Wang on 7/15/24.
+//
+
+import Testing
+@testable import HandTrackingVisualized
+
+struct HandTrackingVisualizedTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
